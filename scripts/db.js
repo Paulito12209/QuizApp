7cf4d53b3a33101b49cd46c37402b2ebdc154d7b
Son Paul <paul.angeles.chaquire@gmail.com>
2025-09-23 15:05:04 +0200

    add bd.js

---
+++ scripts/db.js
@@ -0,0 +1,82 @@
+let questions = [
+  {
+    frage: "Wie deklariert man eine Variable in JavaScript?",
+    antwortA: "var myVar;",
+    antwortB: "int myVar;",
+    antwortC: "let myVar;",
+    antwortD: "const myVar;",
+    richtigeAntwort: "antwortA"
+  },
+  {
+    frage: "Wie gibt man etwas in der Konsole aus?",
+    antwortA: "console.log()",
+    antwortB: "print()",
+    antwortC: "echo()",
+    antwortD: "write()",
+    richtigeAntwort: "antwortA"
+  },
+  {
+    frage: "Welcher Datentyp ist KEIN primitiver Datentyp in JavaScript?",
+    antwortA: "String",
+    antwortB: "Number",
+    antwortC: "Object",
+    antwortD: "Boolean",
+    richtigeAntwort: "antwortC"
+  },
+  {
+    frage: "Wie schreibt man eine Funktion in JavaScript?",
+    antwortA: "function myFunc() {}",
+    antwortB: "def myFunc() {}",
+    antwortC: "func myFunc() {}",
+    antwortD: "function:myFunc() {}",
+    richtigeAntwort: "antwortA"
+  },
+  {
+    frage: "Wie kann man einen Wert aus einem Array abrufen?",
+    antwortA: "array.get(0)",
+    antwortB: "array[0]",
+    antwortC: "array{0}",
+    antwortD: "array(0)",
+    richtigeAntwort: "antwortB"
+  },
+  {
+    frage: "Wie prüft man, ob zwei Werte gleich sind (ohne Typumwandlung)?",
+    antwortA: "==",
+    antwortB: "===",
+    antwortC: "equals()",
+    antwortD: "!=",
+    richtigeAntwort: "antwortB"
+  },
+  {
+    frage: "Wie kommentiert man eine einzelne Zeile in JavaScript?",
+    antwortA: "// Kommentar",
+    antwortB: "# Kommentar",
+    antwortC: "<!-- Kommentar -->",
+    antwortD: "/* Kommentar */",
+    richtigeAntwort: "antwortA"
+  },
+  {
+    frage: "Wie kann man die Länge eines Arrays herausfinden?",
+    antwortA: "array.length",
+    antwortB: "array.size()",
+    antwortC: "array.count",
+    antwortD: "array.length()",
+    richtigeAntwort: "antwortA"
+  },
+  {
+    frage: "Wie kann man eine Schleife von 0 bis 4 schreiben?",
+    antwortA: "for(let i=0; i<5; i++) {}",
+    antwortB: "for(i=0; i<=5; i++) {}",
+    antwortC: "for(let i=1; i<5; i++) {}",
+    antwortD: "for(let i=0; i<4; i++) {}",
+    richtigeAntwort: "antwortA"
+  },
+  {
+    frage: "Wie kann man eine Bedingung prüfen?",
+    antwortA: "if (x == 5) {}",
+    antwortB: "when (x == 5) {}",
+    antwortC: "if x = 5 {}",
+    antwortD: "if x == 5 then {}",
+    richtigeAntwort: "antwortA"
+  }
+];
