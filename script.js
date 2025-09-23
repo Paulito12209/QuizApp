74a3d756245bc100f4718719ffa9113a67e90444
Son Paul <paul.angeles.chaquire@gmail.com>
2025-09-23 21:27:14 +0200

    add card fucntions

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-let currentCustion = 0;
+let currentQuestion = 0;
 
 function init() {
   showQuestion();
 }
 
 function showQuestion() {
-  let question = questions[currentCustion];
+  let question = questions[currentQuestion];
 
   document.getElementById("question-title").innerHTML = question.question;
   document.getElementById("answer_1").innerHTML = question.answer_1;
@@ -13,10 +13,11 @@ function showQuestion() {
   document.getElementById("answer_3").innerHTML = question.answer_3;
   document.getElementById("answer_4").innerHTML = question.answer_4;
   document.getElementById("total-questions").innerHTML = questions.length;
+  showQuestionNumber();
 }
 
 function showAnswer(selection) {
-  let question = questions[currentCustion];
+  let question = questions[currentQuestion];
   selection == question.correctAnswer
     ? // ? console.log(`✅ Richtig!`)
       ((document.getElementById(
@@ -31,4 +32,36 @@ function showAnswer(selection) {
         .getElementById(selection)
         .parentNode.classList.add("text-bg-danger"));
   document.getElementById("next-button").disabled = false;
+}
+
+function nextQuestion() {
+  currentQuestion++;
+  document.getElementById("next-button").disabled = true;
+  resetOptions();
+  showQuestion();
+  showQuestionNumber();
+}
+
+function resetOptions() {
+  document
+    .getElementById("answer_1")
+    .parentNode.classList.remove("text-bg-danger");
+  document.getElementById("answer_1").parentNode.classList.remove("bg-success");
+  document
+    .getElementById("answer_2")
+    .parentNode.classList.remove("text-bg-danger");
+  document.getElementById("answer_2").parentNode.classList.remove("bg-success");
+  document
+    .getElementById("answer_3")
+    .parentNode.classList.remove("text-bg-danger");
+  document.getElementById("answer_3").parentNode.classList.remove("bg-success");
+  document
+    .getElementById("answer_4")
+    .parentNode.classList.remove("text-bg-danger");
+  document.getElementById("answer_4").parentNode.classList.remove("bg-success");
+}
+
+function showQuestionNumber() {
+  let currentQuestionNumber = currentQuestion + 1;
+  document.getElementById("current-question").innerHTML = currentQuestionNumber;
 }
